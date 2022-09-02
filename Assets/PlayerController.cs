a9544ef867cfa6a24105b4b9303e15c2e71eb09c
Anderson2087 <159357tu@gmail.com>
2022-09-02 07:27:22 -0300

    Criando espaco 3D, nave e obstaculos

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int forcaEmX;
+    public int forcaEmZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forcaEmZ * Time.fixedDeltaTime);
+
+        if(Input.GetKey("a") == true){
+            
+            rb.AddForce(-forcaEmX * Time.fixedDeltaTime, 0, 0);
+
+        }
+
+        if(Input.GetKey("d") == true){
+
+            rb.AddForce(forcaEmX * Time.fixedDeltaTime, 0, 0);
+
+        }
+
+    }
+}
